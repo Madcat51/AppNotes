8689a575018549d0b5c42232324bb6c20c0b622e
Madcat51 <51@list.ru>
2021-10-29 08:14:32 +0300

    after convert to kotlin

--- app/src/main/java/site/madcat/appnotes/UI/EditNoteFragment.kt
+++ app/src/main/java/site/madcat/appnotes/UI/EditNoteFragment.kt
@@ -1,147 +1,126 @@
-package site.madcat.appnotes.UI;
+package site.madcat.appnotes.UI
 
-import android.content.Context;
-import android.os.Bundle;
+import android.content.Context
+import android.widget.EditText
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import site.madcat.appnotes.R
+import site.madcat.appnotes.UI.EditNoteFragment
+import site.madcat.appnotes.domain.Note
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+class EditNoteFragment : Fragment() {
+    private var titleEditText: EditText? = null
+    private var detailEditText: EditText? = null
+    private var saveChangeButton: Button? = null
+    private var id = 0
+    private var oldTitle: String? = null
+    private var oldDetail: String? = null
+    private var controller: Controller? = null
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-
-import java.io.Serializable;
-
-import site.madcat.appnotes.R;
-import site.madcat.appnotes.domain.Note;
-
-public class EditNoteFragment extends Fragment {
-    private EditText titleEditText;
-    private EditText detailEditText;
-    private Button saveChangeButton;
-    private int id;
-    private String oldTitle;
-    private String oldDetail;
-    private Controller controller;
-
-    public EditNoteFragment() {
+    internal interface Controller {
+        fun loadList()
+        val screenOrientation: Boolean
+        fun refreshAdapter()
+        fun editNote(id: Int, title: String?, detail: String?)
+        fun addNewNote(title: String?, detail: String?)
     }
 
-    interface Controller {
-        void loadList();
-
-        boolean getScreenOrientation();
-
-        void refreshAdapter();
-
-        void editNote(int id, String title, String detail);
-
-        void addNewNote(String title, String detail);
-    }
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (context instanceof Controller) {
-            controller = (Controller) context;
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is Controller) {
+            controller = context
         } else {
             //todo
         }
     }
 
-    @Override
-    public void onResume() {
-        controller.refreshAdapter();
-        super.onResume();
+    override fun onResume() {
+        controller!!.refreshAdapter()
+        super.onResume()
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_note, container, false);
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit_note, container, false)
     }
 
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        initialsView(view);
-        getParams();
-        saveChangeButton.setOnClickListener(v -> {
-            if (titleEditText.getText() != null | detailEditText.getText() != null) {
-                if (((ListActivity) requireActivity()).newRecord == true) {
-                    controller.addNewNote(titleEditText.getText().toString(), detailEditText.getText().toString());
-                    ((ListActivity) requireActivity()).newRecord = false;
-                    clearView();
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialsView(view)
+        params
+        saveChangeButton!!.setOnClickListener { v: View? ->
+            if (titleEditText!!.text != null!! or detailEditText!!.text != null) {
+                if ((requireActivity() as ListActivity).newRecord == true) {
+                    controller!!.addNewNote(
+                        titleEditText!!.text.toString(),
+                        detailEditText!!.text.toString()
+                    )
+                    (requireActivity() as ListActivity).newRecord = false
+                    clearView()
                 } else {
-                    if ((oldTitle != titleEditText.getText().toString()) | (oldTitle != titleEditText.getText().toString())) {
-                        controller.editNote(id, titleEditText.getText().toString(), detailEditText.getText().toString());
+                    if ((oldTitle !== titleEditText!!.text.toString()) or (oldTitle !== titleEditText!!.text.toString())) {
+                        controller!!.editNote(
+                            id,
+                            titleEditText!!.text.toString(),
+                            detailEditText!!.text.toString()
+                        )
                     }
                 }
-                if (controller.getScreenOrientation() == true) {
-                    controller.loadList();
+                if (controller!!.screenOrientation == true) {
+                    controller!!.loadList()
                 }
             }
-        });
-    }
-
-    public void getParams() {
-        Bundle argument = getArguments();
-        if (argument != null) {
-            Note note = (Note) argument.getSerializable(Note.class.getSimpleName());
-            ((ListActivity) requireActivity()).newRecord = false;
-            getNoteFromList(note);
-        } else {
-            ((ListActivity) requireActivity()).newRecord = true;
         }
     }
 
-    private void clearView() {
-        titleEditText.setText(null);
-        detailEditText.setText(null);
+    val params: Unit
+        get() {
+            val argument = arguments
+            if (argument != null) {
+                val note = argument.getSerializable(Note::class.java.simpleName) as Note?
+                (requireActivity() as ListActivity).newRecord = false
+                getNoteFromList(note)
+            } else {
+                (requireActivity() as ListActivity).newRecord = true
+            }
+        }
+
+    private fun clearView() {
+        titleEditText.setText(null)
+        detailEditText.setText(null)
     }
 
-    private void initialsView(View view) {
-        titleEditText = view.findViewById(R.id.title_edit_text);
-        detailEditText = view.findViewById(R.id.body_edit_text);
-        saveChangeButton = view.findViewById(R.id.save_change_button);
+    private fun initialsView(view: View) {
+        titleEditText = view.findViewById(R.id.title_edit_text)
+        detailEditText = view.findViewById(R.id.body_edit_text)
+        saveChangeButton = view.findViewById(R.id.save_change_button)
     }
 
-    private void getNoteFromList(Note note) {
+    private fun getNoteFromList(note: Note?) {
         if (note != null) {
-            oldTitle = note.getTitle();
-            oldDetail = note.getNoteDetail();
-            titleEditText.setText(note.getTitle());
-            detailEditText.setText(note.getNoteDetail());
-            id = note.getId();
+            oldTitle = note.title
+            oldDetail = note.noteDetail
+            titleEditText!!.setText(note.title)
+            detailEditText!!.setText(note.noteDetail)
+            id = note.id
         }
     }
 
-    public EditNoteFragment setInputArgumentsNoteFrames(Note note) {
-        EditNoteFragment editNoteFragment = new EditNoteFragment();
-        Bundle bundle = new Bundle();
-        bundle.putSerializable(Note.class.getSimpleName(), note);
-        editNoteFragment.setArguments(bundle);
-        return editNoteFragment;
+    fun setInputArgumentsNoteFrames(note: Note?): EditNoteFragment {
+        val editNoteFragment = EditNoteFragment()
+        val bundle = Bundle()
+        bundle.putSerializable(Note::class.java.simpleName, note)
+        editNoteFragment.arguments = bundle
+        return editNoteFragment
     }
-
-
 }
-
-
-
-
-
-
-
